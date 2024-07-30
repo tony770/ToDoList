4e591d5b40a74523abb82ce272981742aab36db0
Tony <tonyzoudev@gmail.com>
2024-07-30 01:24:29 -0400

    addFolder partially working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const homeBtn = document.querySelector('.home');
+const folderBtn = document.querySelector('.folderBtn');
+let lastBtn = homeBtn;
+
+folderBtn.addEventListener('click', () => {
+    const newBtn = document.createElement('button');
+    newBtn.classList.add('projectBtn');
+    newBtn.textContent = 'new';
+    lastBtn.parentNode.insertBefore(newBtn, folderBtn);
+    lastBtn = newBtn;
+})
