b823e442c395c03f58e9dd2c07ef42bfe84d60ef
Tony <tonyzoudev@gmail.com>
2024-08-03 22:37:20 -0400

    add todo modal

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import './style.css';
 import { addFolder, loadButtons, deleteFolder } from './addFolder.js';
+import { hideModal, showModal } from './modal.js';
 
 const homeBtn = document.querySelector('.home');
 const folderBtn = document.querySelector('.folderBtn');
 const projectModal = document.querySelector('.projectModal');
 const submitBtn = document.getElementById('submitName');
-const close = document.getElementsByClassName('close')[0];
+const close = document.getElementsByClassName('close');
+const todoModal = document.querySelector('.todoModal');
+const addListBtn = document.querySelector('.addListBtn');
+const createTodoBtn = document.querySelector('.createTodo');
 
 folderBtn.addEventListener('click', () => {
-    projectModal.style.display = 'flex';
+    showModal(projectModal);
 })
 
-close.onclick = function() {
-    projectModal.style.display = 'none';
+for(let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        const modal = close[i].closest('.modal');
+        hideModal(modal);
+    };
 }
 
 submitBtn.addEventListener('click', () => {
@@ -21,9 +28,18 @@ submitBtn.addEventListener('click', () => {
 
 window.onclick = function(event) {
     if (event.target == projectModal) {
-        projectModal.style.display = 'none';
+        hideModal(projectModal);
+    }
+    else if (event.target == todoModal) {
+        hideModal(todoModal);
     }
 }
+
+addListBtn.addEventListener('click', () => {
+    showModal(todoModal);
+})
+
+
 
 function initializeEventListeners() {
     document.querySelectorAll('.deleteFolder').forEach(button => {
